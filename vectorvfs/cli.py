1a50cd152af61f9d0d126478667632f9dd72bda7
Christian S. Perone <christian.perone@gmail.com>
2025-04-27 09:12:52 +0000

    Fixing top n algorithm.

--- vectorvfs/cli.py
+++ vectorvfs/cli.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import click
 import torch
 from rich.console import Console
+import torch.nn.functional as F
+from heapq import heappush, heappushpop
+
 
 from vectorvfs.utils import PerfCounter
 from vectorvfs.vfsstore import VFSStore, XAttrFile
@@ -42,20 +45,19 @@ def search(n: int, query: str, path: str, force_reindex: bool) -> None:
         with PerfCounter() as model_counter:
             from vectorvfs.encoders import PerceptionEncoder
             pe_encoder = PerceptionEncoder("PE-Core-B16-224")
-            logit_scale = pe_encoder.logit_scale()
 
         console.log(f"Perception Encoder model [bold cyan]{pe_encoder.model_name}[/bold cyan] "
                     f"loaded in [bold cyan]{model_counter.elapsed:.2f}s[/bold cyan].")
 
         status.update("Encoding search query...")
         with PerfCounter() as query_counter:
-            query_features = pe_encoder.encode_text(query).to(torch.float16)
+            query_features = pe_encoder.encode_text(query)
+            query_features = F.normalize(query_features)
 
         console.log(f"Query encoded in [bold cyan]{query_counter.elapsed:.2f}s[/bold cyan].")
         status.update("Processing files...")
 
-        feature_stack = []
-        file_stack = []
+        similarity_heap = []
         for pathfile in Path(path).iterdir():
             if not pathfile.is_file():
                 continue
@@ -68,26 +70,28 @@ def search(n: int, query: str, path: str, force_reindex: bool) -> None:
             vfs_store = VFSStore(xattrfile)
             keys = xattrfile.list()
             if "user.vectorvfs" not in keys or force_reindex:
-                console.log("[bold blue]{pathfile}[/bold blue] not indexed, indexing...")
+                console.log(f"[bold blue]{pathfile}[/bold blue] not indexed, indexing...")
                 features = pe_encoder.encode_vision(pathfile)
+                features = F.normalize(features)
                 features = features.to(torch.float16)
                 vfs_store.write_tensor(features)
             else:
                 features = vfs_store.read_tensor()
-            feature_stack.append(features)
-            file_stack.append(pathfile)
-        
-        feature_stack = torch.vstack(feature_stack)
 
-        with torch.inference_mode():
-            text_probs = feature_stack @ query_features.T
-        
-        text_probs = text_probs.flatten()
-        text_argsort = text_probs.argsort(descending=True)
+            features = features.to(torch.float32)
+            text_probs = features @ query_features.T
+            path_similarity = PathSimilarity(pathfile, text_probs.item())
+
+            if len(similarity_heap) < n:
+                heappush(similarity_heap, path_similarity)
+            else:
+                heappushpop(similarity_heap, path_similarity)
+
+        similarity_heap = sorted(similarity_heap, reverse=True)
         console.log(f"\nTop {n} files found:")
-        for idx in text_argsort[:n]:
-            console.log(f"[bold blue]{file_stack[idx].name}[/bold blue] "
-                        f"(Similarity -> {text_probs[idx]:.3f})")
+        for item in similarity_heap[:n]:
+            console.log(f"[bold blue]{item.path.name}[/bold blue] "
+                        f"(Similarity -> {item.similarity:.3f})")
 
 
 if __name__ == '__main__':
